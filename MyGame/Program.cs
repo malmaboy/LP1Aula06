10f04c9f559654fc51c10088f22ac02a3a2fc58d
Hugo Carvalho <huguinho66@live.com>
2020-04-24 03:29:04 +0100

    ex2

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,9 +6,34 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Enemy enemy = new Enemy("Thor");
-           // Console.WriteLine(Enemy.health); nao da para fazer print pk as variaveis são private
-            Console.WriteLine(enemy.GetName());
+            // Declaração de variaveis 
+            int numEnimies;
+            Enemy[] enemies;
+
+            // Pedir ao utilizador nº de inimgos
+            Console.WriteLine("Insira o nº de inimigos: ");
+            numEnimies = Convert.ToInt32(Console.ReadLine());
+
+            // Para cada inimigo pedir ao utilizador o seu nome,
+            // criar objeto inimigos e guarda-lo num array
+
+            enemies = new Enemy[numEnimies];
+
+            for( int i = 0; i < numEnimies; i++)
+            {
+                string name;
+                Console.WriteLine($"Insira o nome do inimigo #{i}:  ");
+                name = Console.ReadLine();
+                enemies[i] = new Enemy(name); // Guardar num array
+            }
+
+            // Para cada inimigo, imprimir o seu nome
+
+            foreach (Enemy enemy in enemies)
+            {
+                Console.WriteLine($">>{enemy.GetName()}<<");
+            }
+
         }   
 
 
